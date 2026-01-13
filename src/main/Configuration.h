4ac6850244757c9e5473e0188f9daef5bcfa3739
clonix <ondhia.kavy@gmail.com>
2026-01-14 03:19:05 +0530

    Refactor Configuration.h: Added XSHUT pins for I2C addressing and convert thresholds to integers

--- src/main/Configuration.h
+++ src/main/Configuration.h
@@ -16,10 +16,21 @@
 #define PIN_ENC_R_A 19
 #define PIN_ENC_R_B 20
 
-// --- SENSORS (ADC Pins) ---
-#define SENSOR_FRONT_PIN 26
-#define SENSOR_LEFT_PIN  27
-#define SENSOR_RIGHT_PIN 28
+// --- SENSORS (I2C & XSHUT) ---
+// Note: XSHUT pins allow us to set unique I2C addresses for each sensor
+#define SENSOR_FRONT_XSHUT 6   // Connect to XSHUT on Front Sensor
+#define SENSOR_LEFT_XSHUT  7   // Connect to XSHUT on Left Sensor
+#define SENSOR_RIGHT_XSHUT 8   // Connect to XSHUT on Right Sensor
+
+// These are arbitrary unique addresses we will assign during setup()
+#define ADDR_FRONT 0x30
+#define ADDR_LEFT  0x31
+#define ADDR_RIGHT 0x32
+
+// Placeholder IDs for the getDistance() function
+#define SENSOR_FRONT_ID 0
+#define SENSOR_LEFT_ID  1
+#define SENSOR_RIGHT_ID 2
 
 // --- INTERFACE ---
 #define PIN_SDA 4
@@ -27,12 +38,18 @@
 #define PIN_BUTTON 22
 
 // --- RULE-BASED CONSTANTS ---
-#define MAZE_SIZE 10         // Rule: 10x10 unit squares
+#define MAZE_SIZE 10 
 #define SENSOR_SAMPLES 20
-#define WALL_THRESHOLD 12.0  // In cm
+
+// TRON: Changed to integer (mm) for better embedded performance
+#define WALL_THRESHOLD 120    // 120mm (equivalent to your 12.0cm)
+
 #define BASE_SPEED 150
 #define TURN_SPEED 120
 #define STRAIGHT_COST 10
 #define TURN_COST 30
+
+// Movement timing (placeholders until encoders are tuned)
+#define MOVE_TIME_PER_CELL 800 
 
 #endif
